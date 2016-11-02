a292765488a857dcf366145eb2ff83ac5e9e6861
Pataous <braspatrice@gmail.com>
2016-11-02 07:53:17 +0100

    Project_Solar_cell_uniformipy finalized _ It now plots a map of the distribution of solar cell parameters over the full size cell

--- Project/Project_Solar_cell_uniformiPy.py
+++ Project/Project_Solar_cell_uniformiPy.py
@@ -40,13 +40,13 @@ def pos(id):
 class Small_cell:
 	'''This class contains the main solar cell parameters extracted from the IV data, 
 	namely open circuit voltage (Voc), short circuit current (Isc), fill factor (FF) and efficiency (Eta). 
-	The position of the solar cell given by the data parser is also added as an attribute'''
+	The position of the solar cell given by the data parser is  added as an attribute'''
 
 	def __init__(self,file_name):
 		
 		self.file_name = file_name
 
-		''' Extract IV data from a csv file and returns a DataFrame containing the extracted data.
+		''' Data parser _ Extract IV data from a csv file and returns a DataFrame containing the extracted data.
 		Also extracts the position of the cell from the file name'''
 		#print file
 		file_handle = open(self.file_name,'r')
@@ -60,7 +60,8 @@ class Small_cell:
 		file_handle.close()
 
 		'''The file names are supposed to have the format "cell_ID.csv". The cell ID is extracted because it allows to locate the small cell 
-		on the full size cell. This cell ID is converted into a tuple with (x,y) coordinates for easy plotting in the next steps.'''
+		on the full size cell. This cell ID is converted into a tuple with (x,y) coordinates using the "pos" function defined above
+		 for easy plotting in the next steps.'''
 
 		#Considering the assumed format of the file name, the cell ID is extracted
 		self.cell_ID = int(self.file_name[:-4])
@@ -100,6 +101,8 @@ class Small_cell:
 		#Linear fit of I(V) around I=0 to extract Voc
 		fit_param1 = np.polyfit(V_for_fit1, I_for_fit1,1)
 
+		#The fitting equation is in the form y = ax+b. In this case y is the current (I) and x is the voltage (V). 
+		#Voc is the voltage when the current is 0 => Voc = -b/a
 		return -fit_param1[1]/fit_param1[0]
 
 	def Isc(self):
@@ -114,9 +117,10 @@ class Small_cell:
 				V_for_fit2 = [x for x in self.V[v-(fit_range/2)+1 : v+(fit_range/2)+1]]
 				I_for_fit2 = [y for y in self.I[v-(fit_range/2)+1 : v+(fit_range/2)+1]]
 
-		#Linear fit of I(V) around V=0 to extract Isc
+		#Linear fit of I(V) around V=0 to extract Isc. 
 		fit_param2 = np.polyfit(V_for_fit2, I_for_fit2,1)
 
+		#Similar to Voc calculation but this time, the voltage is zero => Isc = b
 		return fit_param2[1]
 
 
@@ -146,8 +150,8 @@ class Small_cell:
 
 	
 class Full_size_cell:
-	''' Extract the solar cell parameters of all 184 small cells in a big cells. The data is stored in 184 .csv files
-	which names are written according to the following format to locate the small cells on the full size cell. All the
+	''' Extract the solar cell parameters of all 184 small cells in a full size cell. The data is stored in 184 .csv files
+	which names are written according to the following format : 'cell_ID.csv' to locate the small cells on the full size cell. All the
 	files are in the same folder called "input_path"'''
 
 	def __init__(self, input_path):
@@ -157,7 +161,7 @@ class Full_size_cell:
 		list_of_files = os.listdir(self.input_path)
 		
 
-		#Initialization of the list will contain a Small_cell object for each file in the directory
+		#Initialization of the list that will contain a Small_cell object for each file in the directory
 		self.small_cells = []
 
 		#go to the directory for data extraction
@@ -170,10 +174,10 @@ class Full_size_cell:
 		# check if the folder only contains file that can be treated by the program
 		for i in list_of_files:
 			if i not in check:
-				raise TypeError('Not all the files in the specified directory are compatible with this program. The file format should be "cell_number.csv"')
+				raise TypeError('Not all the files in the specified directory are compatible with this program. The file format should be "cell_ID.csv"')
 
 		"""create a list of small cells containing data extracted from each csv file in the folder. 
-		The full size cell is not square so coordinates such as (0,0) or (0,1) do not correspond to any small cell."""
+		The full size cell is not square (it has rounded corners) so coordinates such as (0,0) or (0,1) do not correspond to any small cell."""
 		self.small_cells = [Small_cell(f) for f in list_of_files]
 		#print self.small_cells
 
@@ -189,15 +193,15 @@ class Full_size_cell:
 	
 	def plot_Voc_map(self):	
 
-		'''Plot a map representing the distribution of a solar cell parameter. Will be used in the Full_size_cell class.'''
+		'''Plot a map representing the distribution of the open circuit voltage. Will be used in the Full_size_cell class.'''
 
 		# generate 2 2d grids for the x & y bounds
 		x, y = np.mgrid[slice(0, 15, 1), slice(0, 15, 1)]
 
-		#Create two figures to compare two methods. First pcolormesh is tried and then contourf.
+		#Create two figures to compare two methods. First pcolormesh is tried and then contourf.The contourf was chosen eventually.
 		
 
-		#pcolormesh
+		'''#pcolormesh
 		plt.figure(1)
 		im = plt.pcolormesh(x, y, self.Voc_map)
 		c1 = plt.colorbar(im)
@@ -205,11 +209,10 @@ class Full_size_cell:
 		plt.title('Open circuit voltage distribution (1)')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
-		plt.gca().set_aspect('equal')
+		plt.gca().set_aspect('equal')'''
 		
 		#contourfs
 		#arrange the color scale
-		plt.figure(2)
 		step = 0.01
 		levels = np.arange(self.Voc_map.min(),self.Voc_map.max()+step,step)
 
@@ -218,24 +221,24 @@ class Full_size_cell:
 		cf = plt.contourf(x[:-1, :-1], y[:-1, :-1], self.Voc_map[:-1, :-1], levels)
 		c2 = plt.colorbar(cf)
 		c2.ax.set_ylabel('Voc [V]')
-		plt.title('Open circuit voltage distribution (2)')
+		plt.title('Open circuit voltage')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
 		plt.gca().set_aspect('equal')
 
 
-		plt.show()
+		
 
 	def plot_Isc_map(self):
-		'''Plot a map representing the distribution of a solar cell parameter. Will be used in the Full_size_cell class.'''
+		'''Plot a map representing the distribution of the short circuit current. Will be used in the Full_size_cell class.'''
 
 		# generate 2 2d grids for the x & y bounds
 		x, y = np.mgrid[slice(0, 15, 1), slice(0, 15, 1)]
 
-		#Create two figures to compare two methods. First pcolormesh is tried and then contourf.
+		#Create two figures to compare two methods. First pcolormesh is tried and then contourf.The contourf was chosen eventually.
 		
 
-		#pcolormesh
+		'''#pcolormesh
 		plt.figure(1)
 		im = plt.pcolormesh(x, y, self.Isc_map)
 		c1 = plt.colorbar(im)
@@ -243,11 +246,11 @@ class Full_size_cell:
 		plt.title('Short circuit current distribution (1)')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
-		plt.gca().set_aspect('equal')
+		plt.gca().set_aspect('equal')'''
 		
 		#contourfs
 		#arrange the color scale
-		plt.figure(2)
+		
 		step = 0.001
 		levels = np.arange(self.Isc_map.min(),self.Isc_map.max()+step,step)
 
@@ -256,24 +259,24 @@ class Full_size_cell:
 		cf = plt.contourf(x[:-1, :-1], y[:-1, :-1], self.Isc_map[:-1, :-1], levels)
 		c2 = plt.colorbar(cf)
 		c2.ax.set_ylabel('Isc [A]')
-		plt.title('Short circuit current distribution (2)')
+		plt.title('Short circuit current')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
 		plt.gca().set_aspect('equal')
 
 
-		plt.show()
+		
 
 	def plot_FF_map(self):
-		'''Plot a map representing the distribution of a solar cell parameter. Will be used in the Full_size_cell class.'''
+		'''Plot a map representing the distribution of the fill factor. Will be used in the Full_size_cell class.'''
 
 		# generate 2 2d grids for the x & y bounds
 		x, y = np.mgrid[slice(0, 15, 1), slice(0, 15, 1)]
 
-		#Create two figures to compare two methods. First pcolormesh is tried and then contourf.
+		#Create two figures to compare two methods. First pcolormesh is tried and then contourf. The contourf was chosen eventually.
 		
 
-		#pcolormesh
+		'''#pcolormesh
 		plt.figure(1)
 		im = plt.pcolormesh(x, y, self.FF_map)
 		c1 = plt.colorbar(im)
@@ -281,11 +284,11 @@ class Full_size_cell:
 		plt.title('Fill factor distribution (1)')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
-		plt.gca().set_aspect('equal')
+		plt.gca().set_aspect('equal')'''
 		
 		#contourfs
 		#arrange the color scale
-		plt.figure(2)
+		
 		step = 1
 		levels = np.arange(self.FF_map.min(),self.FF_map.max()+step,step)
 
@@ -294,24 +297,24 @@ class Full_size_cell:
 		cf = plt.contourf(x[:-1, :-1], y[:-1, :-1], self.FF_map[:-1, :-1], levels)
 		c2 = plt.colorbar(cf)
 		c2.ax.set_ylabel('FF [%]')
-		plt.title('Fill factor distribution (2)')
+		plt.title('Fill factor')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
 		plt.gca().set_aspect('equal')
 
 
-		plt.show()
+		
 
 	def plot_Eta_map(self):
-		'''Plot a map representing the distribution of a solar cell parameter. Will be used in the Full_size_cell class.'''
+		'''Plot a map representing the distribution of the efficiency. Will be used in the Full_size_cell class.'''
 
 		# generate 2 2d grids for the x & y bounds
 		x, y = np.mgrid[slice(0, 15, 1), slice(0, 15, 1)]
 
-		#Create two figures to compare two methods. First pcolormesh is tried and then contourf.
+		#Create two figures to compare two methods. First pcolormesh is tried and then contourf. The contourf was chosen eventually.
 		
 
-		#pcolormesh
+		'''#pcolormesh
 		plt.figure(1)
 		im = plt.pcolormesh(x, y, self.Eta_map)
 		c1 = plt.colorbar(im)
@@ -319,12 +322,12 @@ class Full_size_cell:
 		plt.title('Efficiency distribution (1)')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
-		plt.gca().set_aspect('equal')
+		plt.gca().set_aspect('equal')'''
 		
 		#contourfs
 		#arrange the color scale
-		plt.figure(2)
-		step = 1
+		
+		step = 0.2
 		levels = np.arange(self.Eta_map.min(),self.Eta_map.max()+step,step)
 
 		# contours are *point* based plots, so convert our bound into point
@@ -332,24 +335,50 @@ class Full_size_cell:
 		cf = plt.contourf(x[:-1, :-1], y[:-1, :-1], self.Eta_map[:-1, :-1], levels)
 		c2 = plt.colorbar(cf)
 		c2.ax.set_ylabel('Efficiency [%]')
-		plt.title('Efficiency distribution (2)')
+		plt.title('Efficiency')
 		plt.xlabel('y coordinate')
 		plt.ylabel('x coordinate')
 		plt.gca().set_aspect('equal')
 
 
-		plt.show()
+		
 	
 
 	def sum_plot(self):
+		'''Plot the distribution of all four parameters using contouf function'''
 
-		
+		plt.figure(1)
+
+		plt.subplot(221)
+		self.plot_Voc_map()
+
+		plt.subplot(222)
+		self.plot_Isc_map()
+
+		plt.subplot(223)
+		self.plot_FF_map()
+
+		plt.subplot(224)
+		self.plot_Eta_map()
+
+		plt.show()
 
 
 
-#Test
+
+
+
+
+#Test _  A testing folder for this program is available under python_homework\Project\uniformity_U1140712\raw
 F1 = Full_size_cell('C:\Users\Patrice.Bras\Github\python_homework\Project\uniformity_U1140712\\raw')
+
+#Test _ see if sum_plot has the expected output 
+F1.sum_plot()
+
+#Go back to the project directory
 os.chdir('C:\Users\Patrice.Bras\Github\python_homework\Project')
+
+
 
 
 
